4c514409d07777b4028a72d93d3ffd32998f4610
Shen Liao <shen.liao@thoughtworks.com>
2020-09-15 11:47:45 +0800

    [Nelson Liao]: finish

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,13 @@
-export const getPoetry
+export const getPoetry = () => 
+  fetch("https://v1.jinrishici.com/all.json")
+    .then(response => response.json())
+    .then(data => {
+      const result = [];
+      result.push(data.origin);
+      result.push(data.author);
+      result.push(data.content);
+      return result;
+    })
+    .catch((error) => {
+      return error;
+    });
